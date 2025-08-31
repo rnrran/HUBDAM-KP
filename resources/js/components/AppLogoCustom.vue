36bdc1cacbad10e27982629be2c3b159faec8425
rnrran <randykamalhusen@gmail.com>
2025-08-31 17:08:38 +0800

    fix: logo

--- resources/js/components/AppLogoCustom.vue
+++ resources/js/components/AppLogoCustom.vue
@@ -8,7 +8,7 @@
 export default {
     data() {
         return {
-            logoSrc: require('@/assets/logo-hubdam.jpg'), 
+            logoSrc: require('@/assets/Kapota baru.png'), 
         };
     }
 }
